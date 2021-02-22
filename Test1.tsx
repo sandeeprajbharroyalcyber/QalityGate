7ec2b1f83b607cb38c7e289b29aeafeda7f86be1
Sandeep Rajbhar <sandeep.rajbhar@royalcyber.com>
2021-02-22 16:31:10 +0530

    code line increased

--- Test1.tsx
+++ Test1.tsx
@@ -69,31 +69,6 @@ export class App extends React.Component<Props, State> {
   }
 
 
-  public onIncrement = () => {
-
-    console.log("Testing log ++ ", this.state.count + 1);
-    this.setState({
-      count:  this.state.count + 1,
-    })
-  }
-
-  public onIncrement = () => {
-
-    console.log("Testing log ++ ", this.state.count + 1);
-    this.setState({
-      count:  this.state.count + 1,
-    })
-  }
-
-  public onIncrement = () => {
-
-    console.log("Testing log ++ ", this.state.count + 1);
-    this.setState({
-      count:  this.state.count + 1,
-    })
-  }
-
-
   public onDecremnent = () => {
 
     console.log("Testing log ++ ", this.state.count - 1);
