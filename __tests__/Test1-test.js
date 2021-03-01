e88b8ef47734ef1bca0b48b3b678a9415e570eab
Sandeep Rajbhar <sandeep.rajbhar@royalcyber.com>
2021-03-01 09:44:18 +0530

    added

---
+++ __tests__/Test1-test.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import Test1 from '../Test1';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+describe('Testpage Component', () => {
+  it('renders correctly', () => {
+    renderer.create(<Test1 />);
+  });
+
+  test('test sample function', () => {
+    let test1comp = renderer.create(<Test1 />).getInstance();
+    expect(test1comp.sampleFunction(1, 2)).toEqual(3);
+  });
+});
