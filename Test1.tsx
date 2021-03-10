918294679aeefc6b898720175d17b02423a6914e
Sandeep Rajbhar <sandeep.rajbhar@royalcyber.com>
2021-03-10 09:10:50 +0530

    node module remove

--- Test1.tsx
+++ Test1.tsx
@@ -77,40 +77,7 @@ export class Test1 extends React.Component<Props, State> {
     return x - y;
   }
 
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
-  public sampleFunction3 = (x: any, y: any) => {
-    return x - y;
-  }
-
   
-
 
   public render() {
     return (
